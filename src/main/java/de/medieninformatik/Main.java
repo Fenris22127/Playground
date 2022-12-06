8f91c0c9f5776f9a14548f94e15da99df0e7fc30
u37531 <u37531@hs-harz.de>
2022-12-06 11:43:19 +0100

    Added TODO

--- src/main/java/de/medieninformatik/Main.java
+++ src/main/java/de/medieninformatik/Main.java
@@ -3,6 +3,7 @@ package de.medieninformatik;
 import java.io.FileNotFoundException;
 
 public class Main {
+    //TODO: Add XML to implement scheme
     public static void main(String[] args) {
         try {
             ReadXML.read();
